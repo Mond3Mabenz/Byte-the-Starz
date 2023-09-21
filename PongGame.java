52b026de750c7e24c3bb7e14196cf9d86c72586e
LJaho <125566175+LJaho@users.noreply.github.com>
2023-09-21 19:23:18 +0200

    Update PongGame.java

--- PongGame.java
+++ PongGame.java
@@ -4,31 +4,34 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
 public class PongGame extends JPanel implements MouseMotionListener{
+	//Extends? 
+	//implements
 	
 	static final int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 480;
 	private Ball gameBall;
-	private Paddle userPaddle, pcPaddle;
-	
+	private Paddle userPaddle , pcPaddle;
+    
+	private int userScore , pcScore  ;//this will be the initialiser
 	private int userMouseY;
 	
+	//addMouseMotionListener(this);
+
+
 	public static void Paint(Graphics g){
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 	}
 	
 	public PongGame(){
-		gameBall = new Ball(300, 200, 3, 3, 3, Color.YELLOW, 10);
-		
+		gameBall = new Ball(300, 200, 3, 3, 100, Color.GREEN, 10);
 		userPaddle = new Paddle(10, 200, 75, 3, Color.BLUE);
-		
 		pcPaddle = new Paddle(610, 200, 75, 3, Color.RED);
-		
+
 		userMouseY = 0;
-		
-		//listen for motion events on this object
+		//listen for motion event on this object(the mouse)
 		addMouseMotionListener(this);
 	}
-	
+
 	public void paintComponent(Graphics g){
 		
 		g.setColor(Color.BLACK);
@@ -40,41 +43,69 @@ public class PongGame extends JPanel implements MouseMotionListener{
 		//draws the paddles
 		userPaddle.colour_paddles(g);
 		pcPaddle.colour_paddles(g);
+
+		//updates the score
+		//Under Scoring at Kevin guides for simple 2d game for java
+		//DRAWSTRING METHOD NEEDS A STRING TO PRINT AND LOCATION TO PRINT AT
+		//SORTA LIKE HOW TURLE WORKED BY REPOSITIONING IT THEN SETTING IT DOWN AND DRAWING
+		
+		g.setColor(Color.WHITE);
+		g.drawString("Score - USER [ " + userScore + " ] PC [ " + pcScore + " ] ", 250, 20);
+		//checks if player lost a point? checks if  paddle failed to collide with the ball
+			if(gameBall.getX() < 0){
+				//the player has lost a point
+				pcScore++;
+				resetz();
+			}
+			else if(gameBall.getX() > WINDOW_WIDTH){
+				//THE PC HAS LOST
+				userScore++;
+				resetz();
+			}
 	}
 	
 	public void gameLogic(){
 		//moves the ball one frame
 		gameBall.moveBall();
 		
-		//makes the ball bounce or pass through edges
+		//makes the ball bounce or move through the edges
 		gameBall.bounceOffEdges(0, WINDOW_HEIGHT);
 		
-		//moves the paddle towards where the mouse is
+		//paddles will move to where the balll is
 		userPaddle.moveTo(userMouseY);
-		
-		//moves the PC paddle towards the ball y position
+
+		//moves pc paddle to where the ball y position should be
 		pcPaddle.moveTo(gameBall.getY());
-		
-		//checks if ball has collided with either paddle
+
 		if(pcPaddle.checkCollision(gameBall) || userPaddle.checkCollision(gameBall)){
-			//reverses the ball if it collides with either paddlegame
-			gameBall.reverseX();
-			
+			//moves the ball to move in the opposite direction if it collides with either paddle
+			gameBall.reverseX();		
 		}
-		
 	}
-	
 	public void mouseDragged(MouseEvent e){
-		
-		
-		
+
+
 	}
-	
-	
 	public void mouseMoved(MouseEvent e){
 		userMouseY = e.getY();
-	}
-	
-	
 
+	}
+	//this resets for new rounds so that there isnt a continuation when someone loses a point
+	public void resetz(){
+		//pause for a second or so to give players a breather
+		//for this to work the ball class requires setters
+		try{
+			Thread.sleep(1000);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+
+		gameBall.setX(300);
+		gameBall.setY(200);
+		gameBall.setCx(3);
+		gameBall.setCy(3);
+		gameBall.setSpeed(3);
+		int bounceCount = 0;
+	}
 }
